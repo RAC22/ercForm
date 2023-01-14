ee4b5b3d7ec0a6c2924eeedf19e6d74da1723f7a
RAC22 <robrac83@gmail.com>
2023-01-14 00:59:47 -0800

    more forms

--- erc.js
+++ erc.js
@@ -6,6 +6,69 @@ const warn1 = document.getElementById('average_fulltime_employees_2020_warning')
 const warn2 = document.getElementById('average_fulltime_employees_2021_warning')
 console.log(inputs)
 
+function w2logic () {
+    const w2 = localStorage.getItem('w2_wages_paid')
+    if(w2 == 'true'){
+        document.location.href='./form4.html'
+    }else{
+        document.location.href='./sorry.html'
+    }
+}
+function govLogic () {
+    const gov = localStorage.getItem('government_entity')
+    if(gov == 'true'){
+        document.location.href='./sorry.html'
+    }else{
+        document.location.href='./form5.html'
+    }
+}
+function bizStartLogic () {
+    const startedBefore = localStorage.getItem('started_before_feb_2020')
+    if(startedBefore == 'true'){
+        document.location.href='./empnum.html'
+    }else{
+        document.location.href='./grossrev.html'
+    }
+}
+function fteNumLogic () {
+    const fteNum = localStorage.getItem('fte_2019')
+    if(fteNum == 'OVER_500'){
+        document.location.href='./sorry.html'
+    }else{
+        document.location.href='./qualop.html'
+    }
+}
+function revLogic () {
+    const rev = localStorage.getItem('over_million_gross_receipts')
+    if(rev == 'true'){
+        document.location.href='./qualop.html'
+    }
+    if(rev === 'false'){
+        document.location.href='./startupqual.html'
+    }
+    //TODO finish startupqual qualification
+}
+function revReducLogic () {
+    const revReduced = localStorage.getItem('had_revenue_reduction')
+    if(revReduced == 'false'){
+        document.location.href='./supply.html'
+    }
+    //TODO if true  -> qualify
+}
+function supplyChainLogic () {
+    const supplyDis = localStorage.getItem('supply_chain_disruption')
+    if(supplyDis == 'false'){
+        document.location.href='./lockdown.html'
+    }
+    //TODO if true -> qualify
+}
+function lockdownLogic () {
+    const lockedDown = localStorage.getItem('suspension')
+    if(lockedDown == 'false'){
+        document.location.href='./sorry.html'
+    }
+    //TODO if true -> qualify
+}
 function changeBigMoney () {
     const num2020 = numInput2020.value
     const num2021 = numInput2021.value
@@ -32,16 +95,21 @@ function changeBigMoney () {
 if(numInput2020 || numInput2021){
     window.onkeyup = changeBigMoney
     window.onmouseup = changeBigMoney
+    window.onmousemove = changeBigMoney
 }
 
 function persistInput(inputs) {
     for(const idx in inputs){
         const input = inputs[idx]
+        console.log(input)
         const key = input.name;
         const storedValue = localStorage.getItem(key);
 
-        if (storedValue){
+        if (storedValue && input.type != 'radio'){
         input.value = storedValue;
+        }
+        if(input.type == 'radio' && input.value == storedValue){
+            input.checked = true
         }
         input.addEventListener('input', function () {
             localStorage.setItem(key, input.value);
@@ -51,4 +119,3 @@ function persistInput(inputs) {
 if(inputs.length) {
     persistInput(inputs)
 }
-changeBigMoney();
